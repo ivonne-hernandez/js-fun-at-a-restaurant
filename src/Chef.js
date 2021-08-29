e8f192ecf61e559504211302d7ac073bd6f65727
Ivonne Hernandez <ivonne702@gmail.com>
2021-08-29 13:42:42 -0600

    Complete the functions for the Chef file

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,24 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+  greetCustomer(custName, isMorningTime) {
+   if (isMorningTime) {
+     return `Good morning, ${custName}!`;
+   } else {
+      return `Hello, ${custName}!`;
+   }
+ }
+
+ checkForFood(foodItemObject) {
+   for (var i = 0; i < this.restaurant.menus[foodItemObject.type].length; i++) {
+     if(foodItemObject.name === this.restaurant.menus[foodItemObject.type][i].name) {
+       return `Yes, we're serving ${foodItemObject.name} today!`;
+     }
+   }
+   return `Sorry, we aren't serving ${foodItemObject.name} today.`;
+ }
 }
 
 module.exports = Chef;
